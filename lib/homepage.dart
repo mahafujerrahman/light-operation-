1dee0af7c512d7fa8ba2ce2e9ac6304b4b465091
mahafujer <mahafujer1999@gmail.com>
2024-02-26 19:16:30 +0600

    update homepage

---
+++ lib/homepage.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Hello ",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.green),))
+    );
+  }
+}
