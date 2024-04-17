1a9796615719563caa343e2fdc89174fd2f1cd04
mahafujer <mahafujer1999@gmail.com>
2024-04-17 16:03:16 +0600

    update homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,7 +11,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Hello Bangladesh",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.green),))
+      appBar: AppBar(title: Text("Hello Bangladesh ",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.green),))
     );
   }
 }
